0bd610992e3aed15d187628cdb0711148d40ee88
tnbin847 <tnqls3718@gmail.com>
2024-06-29 23:01:14 +0900

    Initial commit

---
+++ src/test/java/com/tnbin/popri/WebAppTests.java
@@ -0,0 +1,13 @@
+package com.tnbin.popri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
